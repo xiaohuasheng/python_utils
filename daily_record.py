4de788ecec1069333644853fe18b8409203910d8
watson.zeng <watson.zeng@corp.to8to.com>
2019-09-16 09:14:08 +0800

    今天是周末，不生成

--- daily_record.py
+++ daily_record.py
@@ -7,7 +7,17 @@ def get_format_datetime(date_format='%Y-%m-%d'):
     return time.strftime(date_format, time.localtime(time.time()))
 
 
+def is_weekend():
+    which_day = int(time.strftime("%w", time.localtime()))
+    if which_day == 6 or which_day == 0:
+        return True
+    return False
+
+
 def main():
+    if is_weekend():
+        print "今天是周末，不生成"
+        exit(0)
     filename = get_format_datetime() + ".md"
     file_path = os.path.join('/root/dayReflection', filename)
     open(file_path, 'a').close()
