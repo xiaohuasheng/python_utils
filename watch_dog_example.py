9c780a9a8c26d5631a61315adbefeab1bda0a2a4
watson.zeng <watson.zeng@corp.to8to.com>
2019-11-15 17:29:13 +0800

    上传到七牛云

--- watch_dog_example.py
+++ watch_dog_example.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
 import base64
+import random
+import string
 import time
 
 import pyperclip
+import qiniu
 from watchdog.events import *
 from watchdog.observers import Observer
+
+from config import *
 
 
 def write_to_clipboard(a_str):
@@ -34,6 +39,7 @@ class FileEventHandler(FileSystemEventHandler):
             self.global_path = event.src_path
             print event.src_path
             convert_to_base64(event.src_path)
+            # upload_to_qiniu(event.src_path)
 
     # def on_moved(self, event):
     #     if event.is_directory:
@@ -55,21 +61,52 @@ class FileEventHandler(FileSystemEventHandler):
     #
 
 
-def convert_to_base64(filepath):
+def get_filename(filepath):
     dir_name, filename = os.path.split(filepath)
+    filename = filename[7:-4]
+    return filename
+
+
+def convert_to_base64(filepath):
     with open(filepath, "rb") as f:
         base64_data = base64.b64encode(f.read())
-    filename = filename[7:-4]
+    filename = get_filename(filepath)
     html_data = '![%s][%s]\n[%s]:data:image/png;base64,%s' % (filename, filename, filename, base64_data)
     # html_data = '<img src="data:image/jpg;base64,%s"/>' % base64_data
     print html_data
     write_to_clipboard(html_data)
 
 
+# 生成5位随机文件名
+def random_name():
+    return ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(5))
+
+
+# 上传至七牛云
+def upload_img(fn, sfx='.jpg'):
+    key = random_name() + sfx
+    q = qiniu.Auth(QINIU_AK, QINIU_SK)
+    token = q.upload_token(QINIU_BUCKET, key, 3600)
+    ret, info = qiniu.put_file(token, key, fn)
+    if ret != None and ret['key'] == key and ret['hash'] == qiniu.etag(fn):
+        return QINIU_DOMAIN + key
+    else:
+        return False
+
+
+def upload_to_qiniu(filepath):
+    # 'C:\\Users\\watson.zeng\\Pictures\\screenshot\\1.png'
+    res_url = upload_img(filepath)
+    if res_url:
+        filename = get_filename(filepath)
+        a_str = '![%s](%s)' % (filename, res_url)
+        write_to_clipboard(a_str)
+    else:
+        print 'UPLOAD FAILED'
+        exit(0)
+
+
 def test():
-    # filename = "C:\\Users\\watson.zeng\\Pictures\\screenshot\\screen.png"
-    filename = u"C:\\Users\\watson.zeng\\Pictures\\screenshot\\企业微信截图_20191115135954.png"
-    convert_to_base64(filename)
     exit(0)
 
 
